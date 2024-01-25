c6173db9ae32b8e6b210bc03d28b06ccad2a0dbc
jcurtidodev <jcurtidodev@gmail.com>
2024-01-25 17:40:40 +0100

    tablero + estilos

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -1,10 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+const board = Array(9).fill(null)
 
 function App() {
-  return <h1>Tic tac toe</h1>
+  return (
+    <main className='board'>
+      <h1>Tic tac toe</h1>
+      <section className='game'>
+        {
+          board.map((_, index) => {
+            return (
+              <div className='cell' key={index}>
+                <span className='cell-content'>
+                  {index}
+                </span>
+              </div>
+            )
+          })
+        }
+      </section>
+    </main>
+  )
 }
 
 export default App
