430b1bcfef55ca76e06dca210a71918fc1c00ab6
jcurtidodev <jcurtidodev@gmail.com>
2024-01-25 18:12:02 +0100

    useState(tablero) + useState(turno)

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -1,26 +1,67 @@
+import { useState } from "react"
+
 const TURNS = {
   X: 'x',
   O: 'o'
 }
 
-const board = Array(9).fill(null)
+
+const Square = ({children, isSelected, updateBoard, index}) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handelClick = () => {
+    updateBoard(index)
+  }
+
+  return (
+    <div onClick={handelClick} className={className}>
+      {children}
+    </div>
+  )
+}
 
 function App() {
+  const [board, setBoard] = useState(
+    Array(9).fill(null)
+  )
+
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = (index) => {
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
+
   return (
-    <main className='board'>
+    <main className="board">
       <h1>Tic tac toe</h1>
-      <section className='game'>
+      <section className="game">
         {
           board.map((_, index) => {
             return (
-              <div className='cell' key={index}>
-                <span className='cell-content'>
-                  {index}
-                </span>
-              </div>
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
             )
           })
         }
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
       </section>
     </main>
   )
