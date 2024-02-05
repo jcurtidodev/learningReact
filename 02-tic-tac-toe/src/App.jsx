85c2404f74ffca0f810a0b5dabd32be4e7fd77ff
jcurtidodev <jcurtidodev@gmail.com>
2024-02-05 18:49:43 +0100

    guardar partida en localStorage

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -5,13 +5,19 @@ import { TURNS } from "./constants.js"
 import { checkWinnerFrom, checkEndGame } from "./logic/board.js"
 import { WinnerModal } from "./components/WinnerModal.jsx"
 import { Board } from "./components/Board.jsx"
+import { saveGameToStorage, resetGametoStorage } from "./logic/storage/index.js"
 
 function App() {
-  const [board, setBoard] = useState(
-    Array(9).fill(null)
-  )
 
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
   // null = no hay ganador, false = empate
   const [winner, setWinner] = useState(null)
 
@@ -25,6 +31,10 @@ function App() {
     // cambiar turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    // guardar partida
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn})
     // revisar si hay ganador
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
@@ -39,6 +49,8 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGametoStorage()
   }
 
   return (
