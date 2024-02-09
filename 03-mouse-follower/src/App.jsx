05af84c7b76bea1289c24cca1cd65595039630bb
jcurtidodev <jcurtidodev@gmail.com>
2024-02-09 10:56:02 +0100

    update mouse follower

--- 03-mouse-follower/src/App.jsx
+++ 03-mouse-follower/src/App.jsx
@@ -1,8 +1,59 @@
+import { useEffect, useState } from 'react'
 
-function App() {
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('effect', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({x: clientX, y: clientY})
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    } else {
+      setPosition({ x: 0, y: 0 })
+    }
+
+    // cleanup:
+    // -> cuando el componente se desmonta
+    // -> cuando cambian las depependencias, antes de ejecutar el efecto de nuevo
+    return () => {
+      console.log('cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
 
   return (
-    <h3>03 Mouse follower</h3>
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={ () => setEnabled(!enabled)}>
+          {enabled ? 'Desactivar' : 'Activar'} Seguir puntero
+      </button>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
   )
 }
 
