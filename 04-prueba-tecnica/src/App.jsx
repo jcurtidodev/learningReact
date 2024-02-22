8331527e10afeb4af7b7834f768c8a3cff5d5f42
jcurtidodev <jcurtidodev@gmail.com>
2024-02-22 19:24:27 +0100

    separacion de la logica

--- 04-prueba-tecnica/src/App.jsx
+++ 04-prueba-tecnica/src/App.jsx
@@ -1,22 +1,22 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { getRandomFact } from './services/facts.js'
 
-const CAT_ENDOPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDOPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/firstsWords?fontSize=50&fontColor=red&width=500&height=500`
 
 export function App () {
   const [fact, setFact] = useState()
   const [imageUrl, setImgUrl] = useState()
-  
+
   // recuperar los datos
   useEffect(() => {
-    fetch(CAT_ENDOPOINT_RANDOM_FACT)
-      .then(response => response.json())
-      .then(data => {
-        const { fact } = data
-        setFact(fact)
-      })
+    getRandomFact().then(newFact => setFact(newFact))
+    // sería los mismo hacer 
+    // getRandomFact().then(setFact)
+    // ya que le estarías pasando todos los parámetros, aún así no se acaba de considerar buenas prácticas
   }, [])
+
   // setear img según datos
   useEffect(() => {
     if (!fact) return
@@ -25,9 +25,16 @@ export function App () {
     setImgUrl(url)
   }, [fact])
 
+  const handleClick = async () => {
+    const newFact = await getRandomFact()
+    setFact(newFact)
+  }
+
   return (
     <main>
       <h1>App de gatitos</h1>
+
+      <button onClick={handleClick}>Get new fact</button>
       {fact && <p>{fact}</p>}
       {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for ${fact}`} />}
     </main>
