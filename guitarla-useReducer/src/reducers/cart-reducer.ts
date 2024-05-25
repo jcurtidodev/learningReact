a68492cd740a86583ae7e99c7f008ba67bc2ab04
jcurtidodev <jcurtidodev@gmail.com>
2024-05-25 19:54:01 +0200

    definicion metodos cartReducer()

--- guitarla-useReducer/src/reducers/cart-reducer.ts
+++ guitarla-useReducer/src/reducers/cart-reducer.ts
@@ -1,19 +1,61 @@
 import { db } from "../mocks/db"
 import { CartItem, Guitar } from "../types/types"
 
-export type CartActions = 
-{ type: 'add-to-cart', payload: {item: Guitar} } |
-{ type: 'remove-from-cart', payload: {id: Guitar['id']} } |
-{ type: 'decrease-quanitity', payload: {id: Guitar['id']} } |
-{ type: 'increase-quanitity', payload: {id: Guitar['id']} } | 
-{ type: 'clear-cart' }
+export type CartActions =
+  { type: 'add-to-cart', payload: { item: Guitar } } |
+  { type: 'remove-from-cart', payload: { id: Guitar['id'] } } |
+  { type: 'decrease-quanitity', payload: { id: Guitar['id'] } } |
+  { type: 'increase-quanitity', payload: { id: Guitar['id'] } } |
+  { type: 'clear-cart' }
 
 export type CartState = {
   data: Guitar[],
   cart: CartItem[]
 }
 
-export const initialState : CartState = {
+export const initialState: CartState = {
   data: db,
   cart: []
+}
+
+export const cartReducer = (
+  state: CartState = initialState,
+  action: CartActions
+) => {
+  if (action.type === 'add-to-cart') {
+
+    return {
+      ...state
+    }
+  }
+
+  if (action.type === 'remove-from-cart') {
+
+    return {
+      ...state
+    }
+  }
+
+  if (action.type === 'decrease-quanitity') {
+
+    return {
+      ...state
+    }
+  }
+
+  if (action.type === 'increase-quanitity') {
+
+    return {
+      ...state
+    }
+  }
+
+  if (action.type === 'clear-cart') {
+
+    return {
+      ...state
+    }
+  }
+
+  return state
 }
